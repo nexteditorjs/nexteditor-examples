5b791768bdcff0a32155cfa2ed9d66bace988b7b
nexteditorjs <nexteditorjs@gmail.com>
2022-03-03 10:56:24 +0800

    bug fix

--- tools/webhook.js
+++ tools/webhook.js
@@ -37,7 +37,7 @@ handler.on('error', function (err) {
 });
 
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s', event.payload.repository.name, ref);
+  console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
   //
   if (event.payload.repository.name !== 'nexteditor-examples') {
     return;
