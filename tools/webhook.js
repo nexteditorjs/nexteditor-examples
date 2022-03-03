4a376c2fcde458f53790756cf64630a8c7d5d7cb
nexteditorjs <nexteditorjs@gmail.com>
2022-03-03 10:52:30 +0800

    Update webhook.js

--- tools/webhook.js
+++ tools/webhook.js
@@ -1,7 +1,7 @@
-const http = require('http')
+const http = require('http');
 const createHandler = require('github-webhook-handler');
 const path = require('path');
-const handler = createHandler({ path: '/webhook', secret: process.env["PUSH_SECRET"] })
+const handler = createHandler({ path: '/webhook', secret: process.env['PUSH_SECRET'] });
 const spawn = require('child_process').spawn;
 
 function exec(name, path, argv, options) {
@@ -17,27 +17,27 @@ function exec(name, path, argv, options) {
     s.on('close', () => {
       console.log('done');
       resolve();
-    })
+    });
     console.log('has rebuild');
   });
 }
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    console.log(req.url)
-    res.statusCode = 404
-    res.end('no such location')
+http
+  .createServer(function (req, res) {
+    handler(req, res, function (err) {
+      console.log(req.url);
+      res.statusCode = 404;
+      res.end('no such location');
+    });
   })
-}).listen(7777)
+  .listen(7777);
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
+  console.error('Error:', err.message);
+});
 
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref)
+  console.log('Received a push event for %s to %s', event.payload.repository.name, ref);
   //
   if (event.payload.repository.name !== 'nexteditor-examples') {
     return;
@@ -48,12 +48,14 @@ handler.on('push', function (event) {
   exec('pull & build', 'sh', [shFile], {
     cwd: __dirname,
   });
-})
+});
 
 handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
+  console.log(
+    'Received an issue event for %s action=%s: #%d %s',
     event.payload.repository.name,
     event.payload.action,
     event.payload.issue.number,
-    event.payload.issue.title)
-})
+    event.payload.issue.title
+  );
+});
