4f38791155c8c72d20528cfcba819978268a0b3c
nexteditorjs <nexteditorjs@gmail.com>
2022-03-04 10:47:06 +0800

    upgrade

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF1A75',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
