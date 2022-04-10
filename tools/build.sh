464fc1d2bf29bbcdd2e0eda1a42bde31fcc12f65
nexteditorjs <nexteditorjs@gmail.com>
2022-04-10 11:10:34 +0800

    Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -1,43 +1,45 @@
 cd ../
-echo rest code
+echo --------rest code--------
 git reset --hard
-echo pull
+echo --------pull code--------
 git pull
 # rm -rf ./node_modules
 
-echo stop nginx and mysql
+echo --------stop nginx and mysql--------
 service nginx stop
 service mysql stop
 
-echo install dependencies
+echo --------install dependencies--------
 yarn
 
-echo build examples
+echo --------build examples--------
 yarn build
 
-echo copy and move examples
+echo --------copy and move examples--------
 FILE=./dist
 if [ -d "$FILE" ]; then
   rm -rf /var/www/examples
   mv ./dist /var/www/examples
 fi
 
-echo refresh sharedb server
+echo --------refresh sharedb server--------
 cd ../nexteditor-sharedb-server
 git reset --hard
 git pull
 yarn
 tsc
 
-echo refresh yjs
+echo --------refresh yjs--------
 cd ../nexteditor-yjs-server
 git reset --hard
 git pull
 yarn
 
-echo restart node project
+echo --------restart node project--------
 pm2 restart all
 
-echo start nginx and mysql
+echo --------start nginx and mysql--------
 service nginx start
 service mysql start
+
+echo --------done--------
