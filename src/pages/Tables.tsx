c2953492d7072e6c5b3cfe77b2ff497f994dbf1c
nexteditorjs <nexteditorjs@gmail.com>
2022-04-22 12:19:48 +0800

    Update Tables.tsx

--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NextEditor as Editor, LocalDoc } from '@nexteditorjs/nexteditor-core';
+import { NextEditorToolbarHandler } from '@nexteditorjs/nexteditor-ui';
+import { MarkdownInputHandler } from '@nexteditorjs/nexteditor-input-handlers';
 import NextEditor from '../NextEditor';
 
 import initData from './docs/tables.json';
@@ -9,6 +11,8 @@ const defaultDoc = new LocalDoc(initData);
 export default function Tables() {
   //
   const handleCreate = React.useCallback((editor: Editor) => {
+    editor.addCustom('toolbar-handler', (editor) => new NextEditorToolbarHandler(editor));
+    editor.input.addHandler(new MarkdownInputHandler());
     editor.focus();
   }, []);
   //
